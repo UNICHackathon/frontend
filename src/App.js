5f023cdc46d7b110d84bcbb6fe1eb85ce9134b91
Nurlan Suyundukov <hey@suyundukov.com>
2024-10-19 01:21:41 +0300

    Add login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Home from './routes/Home';
 import Income from './routes/Income';
 import Spending from './routes/Spending';
+import Login from "./routes/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/income" element={<Income />}/>
         <Route path="/spending" element={<Spending />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </div>
   );
