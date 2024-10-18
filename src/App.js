45e1aae9e1234165fd4e4ad9971b6b5cd7daa58c
Nurlan Suyundukov <hey@suyundukov.com>
2024-10-19 01:31:20 +0300

    Add path aliases

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Home from './routes/Home';
-import Income from './routes/Income';
-import Spending from './routes/Spending';
-import Login from "./routes/Login";
+import Home from '@routes/Home';
+import Income from '@routes/Income';
+import Spending from '@routes/Spending';
+import Login from '@routes/Login';
 
 function App() {
   return (
